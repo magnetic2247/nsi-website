915bba5dd73a9200f61fbc8fc9f517c2a601768d
polix_minus <polisminus2247@gmail.com>
2021-03-07 14:23:28 +0400

    Added API to Server

---
+++ server.py
@@ -0,0 +1,44 @@
+from bottle import app, route, template, run, static_file, redirect, response
+from bottle.ext.websocket import GeventWebSocketServer, websocket
+import bottle_session
+import backend.helper as h
+
+# Bottle Session
+app = app()
+plugin = bottle_session.SessionPlugin(cookie_lifetime=600)
+app.install(plugin)
+
+# Index
+@route('/')
+def index(session):
+    return template("frontend/index.html", usrnm="World")
+
+# Users
+@route('/api/users/:id')
+def users(session, id):
+    response.set_header('Content-type', 'application/json')
+    if id is not None:
+        return h.users()
+    else:
+        return h.users(id)
+
+# Chats
+@route('/api/chats/:id')
+def chats(session, id):
+    response.set_header('Content-type', 'application/json')
+    if id is not None:
+        return h.chats(id)
+    else:
+        return "Error"
+
+# Messages
+@route('/api/messages/:user_id/:chat_id')
+def messages(session, user_id, chat_id):
+    response.set_header('Content-type', 'application/json')
+    if user_id is not None and chat_id is not None:
+        return h.messages(user_id, chat_id)
+    else:
+        return "Error"
+
+# Server
+run(app=app, host="localhost", port=8080, server=GeventWebSocketServer, debug=True, reloader=True)
